0523684ab1a58f9182db683f1593f6f4e9947355
dallinks <dallinstirling@gmail.com>
2022-02-16 23:38:03 -0700

    Add project files.

---
+++ WaterProject/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterProject.Models;
+using WaterProject.Models.ViewModels;
+
+namespace WaterProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWaterProjectRepository repo;
+        public HomeController(IWaterProjectRepository temp)
+        {
+            repo = temp;
+        }
+        private BookstoreContext context { get; set; }
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+
+            var x = new ProjectsViewModel
+            {
+                Projects = repo.Projects
+                .OrderBy(p => p.ProjectName)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+                PageInfo = new PageInfo
+                {
+                    TotalNumProjects = repo.Projects.Count(),
+                    ProjectsPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            var blah = repo.Projects
+                .OrderBy(p => p.ProjectName)
+                .Skip((pageNum -1) * pageSize)
+                .Take(pageSize);
+            return View(x);
+        }
+
+    }
+}
---
+++ WaterProject/Models/BookstoreContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WaterProject.Models
+{
+    public partial class BookstoreContext : DbContext
+    {
+        public BookstoreContext()
+        {
+        }
+
+        public BookstoreContext(DbContextOptions<BookstoreContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlite("Data Source = Bookstore.sqlite");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
